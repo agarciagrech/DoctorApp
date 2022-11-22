4ef119cb7b9958890f9a47179c0f982af2a51cf7
Angela <agarciagrech@gmail.com>
2022-11-22 12:30:20 +0100

    java fx

---
+++ src/doctorapp/MenuDoctorController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doctorapp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author agarc
+ */
+public class MenuDoctorController {
+    @FXML
+    private Button registerPatientButton;
+
+    @FXML
+    private Button registerDoctorButton;
+
+    @FXML
+    private Button viewPatientsButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    void exit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerDoctor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerPatient(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewPatients(ActionEvent event) {
+
+    }
+
+    
+}
--- src/doctorapp/logInDoctorController.java
+++ src/doctorapp/logInDoctorController.java
@@ -5,10 +5,18 @@
  */
 package doctorapp;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -33,7 +41,14 @@ public class logInDoctorController {
     }
 
     @FXML
-    void logIn(ActionEvent event) {
+    void logIn(ActionEvent event) throws  IOException {
+        URL url = new File("src/doctorapp/menuDoctor.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);    
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
 
     }
     
